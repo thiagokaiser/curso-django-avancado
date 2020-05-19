c6974b9efb82fa20d4dfa054ff911b18b2a2c699
unknown <thiago.kaiser@hotmail.com>
2020-05-19 17:57:35 -0300

    Envio de emails

--- clientes/models.py
+++ clientes/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.mail import send_mail, mail_admins
+from django.template.loader import render_to_string
 
 class Documento(models.Model):
     num_doc = models.CharField(max_length=50)
@@ -13,6 +15,29 @@ class Person(models.Model):
     bio = models.TextField()
     photo = models.ImageField(upload_to='clients_photos', null=True, blank=True)
     doc = models.OneToOneField(Documento, null=True, blank=True, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        super(Person, self).save(*args, **kwargs)
+
+        data = {'cliente': self.first_name}
+        plain_text = render_to_string('emails/novo_cliente.txt', data)
+        html_text = render_to_string('emails/novo_cliente.html', data)
+
+        send_mail(
+            'Novo cliente cadastrado',
+            plain_text,
+            'django@gregorypacheco.com.br',
+            ['thiago.kaiser@hotmail.com'],
+            html_message=html_text,
+            fail_silently=False,
+        )
+
+        mail_admins(
+            'Novo cliente cadastrado',
+            plain_text,
+            html_message=html_text,
+            fail_silently=False,
+        )
 
     class Meta:
         permissions = {
