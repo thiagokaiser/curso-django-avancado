9279f9c679fd8b385d65b43088f32cc4d2109cd1
unknown <thiago.kaiser@hotmail.com>
2020-05-19 15:54:45 -0300

    Criando Middleware personalizado

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mymiddlewares.MetaData.MetaData',
 ]
 
 ROOT_URLCONF = 'gestao_clientes.urls'
---
+++ mymiddlewares/MetaData.py
@@ -0,0 +1,16 @@
+class MetaData(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        if request.user.is_authenticated:
+            message = 'Ola %s, bom dia!' % request.user.username
+        else:
+            message = 'Ola, bom dia'
+
+        request.session['message'] = message
+        return None
